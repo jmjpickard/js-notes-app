7fca5f396897a63064725ce2391f841061fe1689
jmjpickard <jack.pickard@carnallfarrar.com>
2021-03-23 13:06:26 +0000

    crud setup

--- src/index.js
+++ src/index.js
@@ -1,2 +1,36 @@
-// index.js
-// This is the main entry point of our application
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+require('dotenv').config();
+
+// Local module imports
+const db = require('./db');
+const models = require('./models');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+
+// Run our server on a port specified in our .env file or port 4000
+const port = process.env.PORT || 4000;
+const DB_HOST = process.env.DB_HOST;
+
+const app = express();
+
+db.connect(DB_HOST);
+
+// Apollo Server setup
+const server = new ApolloServer({ 
+    typeDefs, 
+    resolvers,
+    context: () => {
+        // add db models to the context
+        return { models };
+    } 
+});
+
+// Apply the Apollo GraphQL middleware and set the path to /api
+server.applyMiddleware({ app, path: '/api' });
+
+app.listen({ port }, () =>
+  console.log(
+    `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+  )
+);
